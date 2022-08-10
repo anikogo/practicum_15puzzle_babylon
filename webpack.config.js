3721a4c2ab425bd0ef31be7850c86923a112a2e0
Artyom Nikogosov <kingartyom@gmail.com>
2022-08-11 01:25:22 +0400

    init commit
    add eslint
    
    test precommit
    
    test precommit
    
    init project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './www/index.html'
+    })
+  ]
+};
