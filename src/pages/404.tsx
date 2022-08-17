7961a927d68d8d3247fff487c42405105c182c75
Sergei Baranov <dev@rkkmkkfx.ru>
2022-08-18 00:04:25 +0400

    App Routing: * 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,14 @@ import Content from '../components/Content';
 export default function Page404() {
   return (
     <Content>
-      <h1>404</h1>
+      <div className="scene">
+        <div className="tiles">
+          <div className="square s0">4</div>
+          <div className="square s1">0</div>
+          <div className="square s2">4</div>
+        </div>
+        <span className="text-[55px] uppercase">Not Found</span>
+      </div>
     </Content>
   );
 }
