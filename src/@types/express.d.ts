2e22062744264290a721c22bfec82a6dda0c7406
Sergei Baranov <dev@rkkmkkfx.ru>
2022-10-08 03:01:35 +0400

    types fixes

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unused-vars */
+// @ts-ignore
 namespace Express {
   interface Request {
+    // @ts-ignore
     user?: string | JwtPayload;
   }
 }
