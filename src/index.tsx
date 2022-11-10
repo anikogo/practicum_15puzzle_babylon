b564d1badb5dd869c066613353441ec6a910579f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2022-11-10 23:29:25 +0300

    feat: sw (#19)

--- src/index.tsx
+++ src/index.tsx
@@ -23,3 +23,17 @@ ReactDOM.hydrate(
   </ThemeContext.Provider>,
   document.getElementById('app'),
 );
+
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch((error: string) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+    });
+  }
+}
+
+startServiceWorker();
