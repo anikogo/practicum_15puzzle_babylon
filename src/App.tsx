9f390f27efaea1a8aa8797bb000908df3f5e6e3c
Bogdan <b@bmazur.ru>
2023-02-03 18:39:20 +0300

    fix: fullscreen & theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
+import { useEffect, useMemo, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ErrorBoundaryWrapper from './components/ErrorBoundaryWrapper';
 
 import routes from './routes';
+import ThemeContext from './context/ThemeContext';
 
 export default function App(): JSX.Element {
+  const [style, setStyle] = useState(false);
+  const [fullScreen, setFullScreen] = useState(false);
+  const providerValue = useMemo(() => ({ style, setStyle }), [style, setStyle]);
+  const fullScreenAction = (event: KeyboardEvent) => {
+    if (event.code === 'KeyF' && event.shiftKey) {
+      setFullScreen(!fullScreen);
+    }
+  };
+  const makeFullScreen = (el: HTMLElement | null) => {
+    if (!document.fullscreenElement) {
+      el?.requestFullscreen();
+      return;
+    }
+    document.exitFullscreen();
+  };
+
+  useEffect(() => {
+    const userTheme = localStorage.getItem('theme');
+
+    if (userTheme === 'true') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+
+    makeFullScreen(document.querySelector('.app'));
+    setStyle(userTheme === 'true');
+    document.addEventListener('keydown', fullScreenAction);
+
+    return () => {
+      document.removeEventListener('keydown', fullScreenAction);
+    };
+  }, [style, fullScreen]);
+
   return (
-    <ErrorBoundaryWrapper>
-      <div className="bg-gradient-to-b from-orange-100 to-orange-50 dark:from-gray-100 dark:to-gray-50">
-        <Routes>
-          {routes.map(({ fetchData, ...routeProps }) => (
-            <Route key={routeProps.path} {...routeProps} />
-          ))}
-        </Routes>
-      </div>
-    </ErrorBoundaryWrapper>
+    <ThemeContext.Provider value={providerValue}>
+      <ErrorBoundaryWrapper>
+        <div className="bg-gradient-to-b from-orange-100 to-orange-50 dark:from-gray-100 dark:to-gray-50">
+          <Routes>
+            {routes.map(({ fetchData, ...routeProps }) => (
+              <Route key={routeProps.path} {...routeProps} />
+            ))}
+          </Routes>
+        </div>
+      </ErrorBoundaryWrapper>
+    </ThemeContext.Provider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,49 +5,34 @@ import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store, history } from './store';
-import ThemeContext from './context/ThemeContext';
 
 import App from './App';
 
 import './styles.css';
 
-let fullScreen = false;
+// let fullScreen = false;
 
 hydrateRoot(
   document.getElementById('app') as HTMLElement,
-  <ThemeContext.Provider value="light">
-    <StrictMode>
-      <Provider store={store}>
-        <HistoryRouter history={history}>
-          <App />
-        </HistoryRouter>
-      </Provider>
-    </StrictMode>
-  </ThemeContext.Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <HistoryRouter history={history}>
+        <App />
+      </HistoryRouter>
+    </Provider>
+  </StrictMode>,
 );
 
-function startServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch((error: string) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-    });
-  }
-}
+// function startServiceWorker() {
+//   if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }).catch((error: string) => {
+//         console.log('ServiceWorker registration failed: ', error);
+//       });
+//     });
+//   }
+// }
 
-document.addEventListener('keydown', (event: KeyboardEvent) => {
-  if (event.code === 'KeyF' && event.shiftKey) {
-    fullScreen = !fullScreen;
-  }
-
-  if (fullScreen) {
-    document.documentElement.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-});
-
-startServiceWorker();
+// startServiceWorker();
